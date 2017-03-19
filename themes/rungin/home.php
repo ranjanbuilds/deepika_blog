02c4342a7e0a714c0a8d65e8f099cb12953e5200
Ranjan Subbiah <rsubs03@gmail.com>
2017-03-19 19:52:19 -0400

    First post home full width and others are masonaried-ish

--- themes/rungin/home.php
+++ themes/rungin/home.php
@@ -15,20 +15,33 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main" data-columns>
-
+		<main id="main" class="site-main" role="main">
+			
 			<?php
 			while ( have_posts() ) : the_post();
-
-				get_template_part( 'template-parts/content', 'page' );
+				if ( $wp_query->current_post == 0 && !is_paged() ):
+					get_template_part( 'template-parts/content', 'preview' ); 
+				endif;
 
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
 					comments_template();
 				endif;
-
 			endwhile; // End of the loop.
 			?>
+			<div id="smallPreview" data-columns>
+			<?php
+			while ( have_posts() ) : the_post();
+				if ( $wp_query->current_post !== 0 ):
+				get_template_part( 'template-parts/content', 'preview' );
+				endif;
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+			endwhile; // End of the loop.
+			?>
+			</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
--- themes/rungin/template-parts/content-preview.php
+++ themes/rungin/template-parts/content-preview.php
@@ -10,7 +10,6 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	
 	<header class="entry-header">
 		
 		<div class="category-decorated">
